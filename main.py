b7ef08806535c6e563e28a1c2631eee39a199dc5
Joguilherme1999 <joguilherme1999@gmail.com>
2025-02-11 17:03:15 -0300

    Add report generator function

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ def main():
     book_path = "books/frankenstein.txt"
     text = book_text(book_path)
     num_words = num_of_words(text)
-    print(f"{num_words} words found in the document")
     characters_count = num_of_characters(text)
-    print(characters_count)
+    report = report_generator(characters_count, num_words, book_path)
+    print(report)
+
+def book_text(path):
+    with open(path) as f:
+        return f.read()
 
 def num_of_words(text):
     words = text.split()
@@ -21,13 +25,22 @@ def num_of_characters(text):
             characters_dict[character] = 1
     return characters_dict
 
-      
+def sort_on(dict):
+    return dict["num"]
 
-def book_text(path):
-    with open(path) as f:
-        return f.read()
-    
-  
+
+def report_generator(characters_count, num_words, book_path):
+    characters_list = []
+    for c in characters_count:
+        if c.isalpha() == True:
+            characters_list.append({"character": c, "num": characters_count[c]})
+    characters_list.sort(reverse=True, key=sort_on)
+    report = f"--- Begin report of {book_path} ---\n"
+    report += f"{num_words} words found in the document\n\n"
+    for i in characters_list:
+        report += f"The '{i["character"]}' character was found {i["num"]} times\n"
+    report += f"\n --- End report ---"
+    return report
 
 
 if __name__ == "__main__":
